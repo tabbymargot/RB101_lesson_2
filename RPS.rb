4ecf2c6a0eaacc3c13e56c397c0e429d680147ff
Caroline Wood <ec2-user@ip-172-31-34-133.eu-north-1.compute.internal>
2023-12-10 11:20:45 +0000

    Working code for program using variables for winning and losing values

--- RPS.rb
+++ RPS.rb
@@ -6,8 +6,54 @@ end
 
 
 
-def win?(first, second)
+# def player_win?(first, second, lose)
   
+#   (first && ( (second == lose[0]) || (second == lose[1]) ) )
+ 
+# end
+
+# def computer_win?(first, second, lose)
+  
+#   (first && ( (second == lose[0]) || (second == lose[1]) ) )
+ 
+# end
+
+# def display_result(player, computer, loses_against_player)
+#   # If the player's choice followed by the computer's choice
+#   # matches one of the expressions in win?, win? will return
+#   # true and You won! will be output.
+#   if player_win?(player, computer, loses_against_player)
+#     prompt("You won!")
+
+#   # If the computer's choice followed by the player's choice
+#   # matches one of the expressions in win?, win? will return
+#   # true and Computer won! will be output.
+#   # elsif computer_win?(computer, player, loses_against_computer)
+#   #   prompt("Computer won!")
+#   else
+#     puts "Didn't work"
+#     # prompt("It's a tie!")
+#   end
+# end
+
+loop do
+  player_choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    player_choice = Kernel.gets().chomp()
+
+    if VALID_CHOICES.include?(player_choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  prompt("You chose: #{player_choice}; Computer chose: #{computer_choice}")
+  
+  # Keys are winning choices, values are losing choices
   winners_losers = {
   'rock' => ['scissors', 'lizard'],
   'paper' => ['rock', 'spock'],
@@ -16,48 +62,37 @@ def win?(first, second)
   'spock' => ['scissors', 'rock']
   }
   
-  (first == 'rock' && ( (second == winners_losers.fetch('rock')[0]) || (second == winners_losers.fetch('rock')[1]) ) ) ||
-    (first == 'paper' && ( (second == winners_losers.fetch('paper')[0]) || (second == winners_losers.fetch('paper')[1]) ) ) ||
-    (first == 'scissors' && ( (second == winners_losers.fetch('scissors')[0]) || (second == winners_losers.fetch('scissors')[1]) ) ) ||
-    (first == 'lizard' && ( (second == winners_losers.fetch('lizard')[0]) || (second == winners_losers.fetch('lizard')[1]) ) ) ||
-    (first == 'spock' && ( (second == winners_losers.fetch('spock')[0]) || (second == winners_losers.fetch('spock')[1]) ) )
-end
+  loses_against_player = ''
+  loses_against_computer = ''
 
-def display_result(player, computer)
-  # If the player's choice followed by the computer's choice
-  # matches one of the expressions in win?, win? will return
-  # true and You won! will be output.
-  if win?(player, computer)
+  # Set the array containing the losing values agains the player choice.
+  winners_losers.select { |key, value|
+    if key == player_choice
+      loses_against_player = value
+    end
+  }
+  # Set the array containing the losing values agains the computer choice.
+  winners_losers.select { |key, value|
+    if key == computer_choice
+      loses_against_computer = value
+    end
+  }
+  
+  if player_choice &&
+    ( 
+      (computer_choice == loses_against_player[0]) || 
+      (computer_choice == loses_against_player[1]) 
+    )
     prompt("You won!")
-
-  # If the computer's choice followed by the player's choice
-  # matches one of the expressions in win?, win? will return
-  # true and Computer won! will be output.
-  elsif win?(computer, player)
+  elsif computer_choice && 
+    ( 
+      (player_choice == loses_against_computer[0]) || 
+      (player_choice == loses_against_computer[1]) 
+    )
     prompt("Computer won!")
   else
-    prompt("It's a tie!")
+    puts "It's a tie!"
   end
-end
-
-loop do
-  player = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    player = Kernel.gets().chomp()
-
-    if VALID_CHOICES.include?(player)
-      break
-    else
-      prompt("That's not a valid choice.")
-    end
-  end
-
-  computer = VALID_CHOICES.sample
-
-  prompt("You chose: #{player}; Computer chose: #{computer}")
-
-  display_result(player, computer)
 
   prompt("Do you want to play again?")
   answer = Kernel.gets().chomp()
